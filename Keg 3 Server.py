a74a2c761ea6132236f8d760e3c9e1ebca143a87
L200184033 <45026412+L200184033@users.noreply.github.com>
2018-12-12 21:50:47 +0700

    Praktikum 10
    Praktikum 10

--- Keg 3 Server.py
+++ Keg 3 Server.py
@@ -1,23 +1,29 @@
 import socket
-def persegi(s=0):
-    return s*s
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("", 50020))
+s.bind(("",50005))
 s.listen(5)
-print "Server penjawab otomatis sudah siap"
-data = ''
-while data.lower() != 'keluar':
+print"Server sudah siap"
+perintah = "
+r=0
+while perintah !='keluar':
     komm, addr = s.accept()
-    while data.lower() !='keluar':
-        data = komm.recv(1024)
-        print 'Pesan:', data
-        if data.find("parameter")!= -1:
-            param,value= data.split("=")
-            if param == "parameter sisi":
-                s = float(value)
-                b = value
-                komm.send("parameter dicatat")
-        elif data == 'hitung':
-            komm.send('luas persegi dengan sisi {} adalah {}'.format (b,persegi(s)))
+    while perintah !='keluar':
+        data = komm.recv(1024).lower().split("=")
+        perintah = data[0]
+        if perintah == 'keluar':
+            s.close()
+            break
+        print 'Pesan:', perintah
+        if len(data) == 1:
+            if perintah == 'jari-jari':
+                r = int(data[1])
+                respon = "jari-jari dicatat"
+                komm.send(respon)
+            else:
+                komm.send('pesan tidak diketahui')
+        elif perintah == 'hitung':
+            L = float(3*22/7*r*r)
+            respon = "Luas bola dengan jari-jari {} adalah {}".format(r,L)
+            komm.send(respon)
         else:
-            komm.send('tidak ada')
+            komm.send('pesan tidak diketahui')
