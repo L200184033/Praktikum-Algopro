f9683a033dff6ce2f75d92146e7a64ef433fc558
L200184033 <45026412+L200184033@users.noreply.github.com>
2018-12-12 21:42:56 +0700

    Update Keg 3 Client.py

--- Keg 3 Client.py
+++ Keg 3 Client.py
@@ -1,18 +1,15 @@
 import socket
 hostname = 'localhost'
-pesan = ''
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((hostname, 50020))
-print "menghitung luas persegi"
-while pesan != 'keluar':
-    pesan = raw_input('pesan :  ')
+pesan = "
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect((hostname, 50005))
+print"Menghitung luas bola"
+while pesan.lower() !='keluar':
+    pesan = raw_input('Pesan:')
     s.send(pesan)
-    if pesan.lower() == 'keluar':
+    if pesan.lower() !='keluar':
         response = s.recv(1024)
-        print 'respon: -'
-        s.close()
-        break
-    elif pesan.lower() != 'keluar':
-        response = s.recv(1024)
-        print 'respon:', response
+        print 'Respon:',response
+    else:
+        print"Respon:-"
 s.close()
